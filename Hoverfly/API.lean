d7d4f5019d660cef2bb89bc97b92a85680e87b70
cassiatorczon <cassiatorczon@gmail.com>
2026-01-18 15:21:31 -0500

    stubbed lean part

---
+++ Hoverfly/API.lean
@@ -0,0 +1,57 @@
+import ProofWidgets
+namespace Step
+
+namespace ProofTree
+
+-- inductive Status : Type where
+--   | selected -- status for current single node selected
+--   | semiselected  -- status for all ancestors of selected node
+--   | unselected -- status for all other visible nodes
+--   -- | hidden -- status for hidden children
+open Lean ProofWidgets
+
+def ID := String
+
+mutual
+structure Goal where
+  id : ID
+  name : String
+  -- status : Status
+  children : List Tactic
+  completed : Bool
+deriving Server.RpcEncodable
+
+structure Tactic where
+  id : ID
+  name : String
+  -- status : Status
+  children : List Goal
+deriving Server.RpcEncodable
+
+end
+
+
+end ProofTree
+
+namespace API
+
+/-
+List of operations:
+
+Change node selection status (selected, unselected, semiselected, hidden?)
+Mark goal node and applicable ancestors as completed
+
+Show applicable tactics for a goal
+Show subgoals for a tactic
+Hide and cache subtree rooted at goal
+Hide and cache subtree rooted at tactic
+Restore subtree rooted at goal
+Restore subtree rooted at tactic
+Hide unexplored neighboring tactics
+
+-/
+
+
+end API
+
+end Step
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Hoverfly
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
