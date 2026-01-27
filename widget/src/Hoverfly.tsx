86bad89d93207266fb9d2fee261f29272f002a43
cassiatorczon <cassiatorczon@gmail.com>
2026-01-26 19:58:34 -0500

    made nodes readonly, added some checking functionality

--- widget/src/Hoverfly.tsx
+++ widget/src/Hoverfly.tsx
@@ -8,32 +8,38 @@ type Kind = "tactic" | "goal"
 
 type Status = "selected" | "semiselected" | "unselected"
 
-type Node = {
+type MutableNode = {
   kind: Kind, // tactic or goal
-  id: ID, // should be unique among all nodes
+  id: ID, // should be unique among all nodes; must have an immutable type
   data: string, // Lean-recognizable description
-  final: boolean, // whether a goal is completed/a tactic is in the final script
+  completed: boolean, // a completed goal or tactic with all completed subgoals
   children: Node[], // applicable tactics for a goal, subgoals for a tactic
   status: Status, // display information
   visible: boolean // visibility in display
 }
 
-// type Node = Readonly<MutableNode>
+type Node = Readonly<MutableNode>
+
+function assert(p: boolean, e: string): void {
+  if (!p) {
+    throw new Error(e)
+  }
+}
 
 function updateNode(
   root: Node,
   update: (n: Node) => Node,
-  breakAfterNode: ((t: Node) => boolean)):
+  breakAfter: ((t: Node) => boolean)):
   Node {
   const newRoot = update(root);
 
-  if (breakAfterNode(root)) {
+  if (breakAfter(root)) {
     return newRoot
   }
 
   return {
     ...newRoot, children: newRoot.children.map(g =>
-      updateNode(g, update, breakAfterNode))
+      updateNode(g, update, breakAfter))
   }
 }
 
@@ -50,12 +56,12 @@ function renderNode(n: Node, onClick: (id: ID) => void): React.ReactNode {
     </Fragment>)
 }
 
-function changeNodeVisibility(n: Readonly<Node>, visible: boolean): Node {
-  return { ...n, visible: true }
+function changeNodeVisibility(n: Node, newVis: boolean): Node {
+  return { ...n, visible: newVis }
 }
 
 function changeStatusAtSelected(root: Node, newStatus: Status): Node {
-  const update = (n: Node) => n.status == 'selected'
+  const update = (n: Node) => n.status === 'selected'
     ? { ...n, status: newStatus } : n
   const breakAfter = (n: Node) => n.status === 'selected'
   return updateNode(root, update, breakAfter)
@@ -68,7 +74,7 @@ function changeStatusAtId(root: Node, id: ID, newStatus: Status): Node {
   return updateNode(root, update, pred);
 }
 
-function isNonstrictAncestorOf(parentCand: Readonly<Node>, childId: ID)
+function isNonstrictAncestorOf(parentCand: Node, childId: ID)
   : boolean {
   if (parentCand.id === childId) {
     return true
@@ -78,7 +84,7 @@ function isNonstrictAncestorOf(parentCand: Readonly<Node>, childId: ID)
   }
 }
 
-function nearestCommonAncestorWithSelected(n: Readonly<Node>, id: ID):
+function nearestCommonAncestorWithSelected(n: Node, id: ID):
   Readonly<Node> {
   const selectedNonstrictAncestor = n.children.find(
     (c: Node) => c.status === 'semiselected' || c.status === 'selected')
@@ -91,47 +97,52 @@ function nearestCommonAncestorWithSelected(n: Readonly<Node>, id: ID):
       return n
     }
   } else {
-    // Note: this should only happen if n is selected
+    assert(n.status === 'selected',
+      "Malformed tree: [" + n.data + "] is not selected and has no selected or"
+      + " semiselected immediate children")
     return n
   }
 }
 
 function handleTacticClick(root: Node, clicked: Node): Node {
   const previouslyExplored = false //TODO
-  const previousNodeWasParent = true //TODO
-  const previousNodeWasDescendant = false //TODO
 
-  const nca = nearestCommonAncestorWithSelected(root, clicked.id);
-  if (nca.id === clicked.id) {
-
+  if (clicked.status === 'selected') {
+    // User has clicked the already-selected node. Do nothing.
+    return root
   }
+  const nca = nearestCommonAncestorWithSelected(root, clicked.id)
 
+  var newGoal;
   if (previouslyExplored) {
     // TODO
     // restore status of subtree when abandoned
     // including which node was selected
-    return root
+    newGoal = root
   } else {
     // change node status to "selected"
-    var newGoal = changeStatusAtId(root, clicked.id, 'selected')
+    var newGoal1 = changeStatusAtId(root, clicked.id, 'selected')
     // TODO
     // show subgoals
-    // if none, retrace path upward marking applicable nodes as final
-    return newGoal
+    // if none, retrace path upward marking applicable nodes as completed
+    newGoal = newGoal1
   }
 
-  if (previousNodeWasParent) {
-    // note: if the previous node was a non-parent ancestor, the
-    // current node should never have been clickable
-    // change parent to semiselected
-    changeStatusAtSelected(root, 'semiselected')
-  } else if (previousNodeWasDescendant) {
+  if (nca.id === clicked.id) {
+    // previously selected node was a descendant of clicked node
     // Change status of all nodes in applicable child subtree of current node to “unselected”
     // Hide that subtree, with:
     // Ellipsis
     // Caching
     // Marking for completeness retained
-    return root //TODO
+    newGoal = root //TODO
+  } else if (nca.status === 'selected') {
+    // if the previous node was a non-parent ancestor, the
+    // current node should never have been clickable
+    assert(nca.children.some((c: Node) => c.id === clicked.id),
+      "Non-child descendant of selected node should not be clickable.")
+    // change parent to semiselected
+    changeStatusAtSelected(root, 'semiselected')
   } else {
     //Change status of neighboring ancestor of that node to “unselected”
     // Hide neighboring ancestor subtree, with:
@@ -147,19 +158,58 @@ function handleTacticClick(root: Node, clicked: Node): Node {
   return root
 }
 
-function treeValid(root: Readonly<Node>): boolean {
-  /* Incomplete list of invariants:
-  - all IDs are unique
-  - all children of goals are tactics
-  - all children of tactics are goals
-  - exactly one node is selected
-  - no grandchildren of selected node are visible
-  - no "cousins" of selected node are visible
-  - immediate children and all ancestors of selected goal
-    are visible (and selected goal itself)
-  - ancestor of selected goal iff 'semiselected'
-  - a goal is completed iff all its descendant goals are */
-  return true // TODO
+function checkUniqueIDs(root: Node, ids: Set<ID>): void {
+  assert(!ids.has(root.id), "Malformed tree: duplicate ID (" + root.id + ").")
+
+  const newIds = ids.add(root.id)
+  for (var c of root.children) {
+    checkUniqueIDs(c, newIds)
+  }
+}
+
+function checkKinds(root: Node, expectedKind: Kind): void {
+  assert(root.kind === expectedKind, "Malformed tree: expected node " + root.id
+    + " to have kind " + expectedKind + ", but found kind " + root.kind + ".")
+
+  const nextKind = expectedKind === 'goal' ? 'tactic' : 'goal'
+  for (var c of root.children) {
+    checkKinds(c, nextKind)
+  }
+}
+
+function checkStatusAndVisibility(root: Node): void {
+  // TODO Harry :)
+}
+
+function checkCompletedness(root: Node): void {
+  let shouldBeCompleted = root.kind === 'goal'
+    ? root.children.some((c: Node) => c.completed)
+    : root.children.every((c: Node) => c.completed)
+  assert(shouldBeCompleted === root.completed,
+    "Incorrect 'completed' field for node " + root.id + "; expected "
+    + shouldBeCompleted + " but found " + root.completed + ".")
+}
+
+function checkTree(root: Node): void {
+  // root is a goal
+  // all goals have only tactic children
+  // all tactics have only goal children
+  checkKinds(root, 'goal')
+
+  // all IDs are unique
+  checkUniqueIDs(root, new Set<ID>())
+
+  // exactly one node is selected
+  // no grandchildren of selected node are visible
+  // no "cousins" of selected node are visible
+  // immediate children and all ancestors of selected goal
+  //   are visible (and selected goal itself)
+  // ancestor of selected goal iff 'semiselected'
+  checkStatusAndVisibility(root)
+
+  // a goal is completed iff at least one of its children is completed
+  // a tactic is completed iff it has no uncompleted children
+  checkCompletedness(root)
 }
 
 function handleGoalClick(root: Node, clickedId: ID): Node {
