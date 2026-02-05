b59f0ba403840000db717e74f160e71567fa84e1
cassiatorczon <cassiatorczon@gmail.com>
2026-02-05 17:29:23 -0500

    some of the termination proofs

--- Hoverfly/API.lean
+++ Hoverfly/API.lean
@@ -1,11 +1,8 @@
 import ProofWidgets
+import Hoverfly.Backend
 
--- TODO namespaces
+namespace API
 open Lean ProofWidgets
-
--- -- TODO should these be the same
--- def GoalId := String
--- def String := String
 
 instance : ToJson String := inferInstanceAs (ToJson String)
 instance : FromJson String := inferInstanceAs (FromJson String)
@@ -15,8 +12,8 @@ instance : FromJson String := inferInstanceAs (FromJson String)
 
 mutual
 structure Goal where
-  id : String
-  data : String
+  id : String -- TODO type
+  data : String -- TODO type
   children : List Tactic
   deriving ToJson, FromJson
 
@@ -37,80 +34,73 @@ instance : Server.RpcEncodable Tactic where
 
 open Lean ProofWidgets Server
 
+mutual
+def backendGoalToAPIGoal (g : Backend.Goal) : Goal :=
+  match g with
+  | Backend.Goal.Goal id data children =>
+    {id := id, data := data, children := children.map (fun t => backendTacticToAPITactic t)}
+termination_by (sizeOf g)
+
+def backendTacticToAPITactic (t : Backend.Tactic) : Tactic :=
+  match t with
+  | Backend.Tactic.Tactic id data children =>
+    {id := id, data := data, children := children.map (fun g => backendGoalToAPIGoal g)}
+termination_by (sizeOf t)
+end
+
+@[simp]
+theorem size_of_list_tactic (x : Tactic) (l : List Tactic) (h : x ∈ l) : sizeOf x <= sizeOf l := by
+  induction l <;> simp_all
+  cases h <;> simp_all <;> omega
+
+@[simp]
+theorem size_of_list_goal (x : Goal) (l : List Goal) (h : x ∈ l) : sizeOf x <= sizeOf l := by
+  induction l <;> simp_all
+  cases h <;> simp_all <;> omega
+
+mutual
+def APIGoaltoBackendGoal (g : Goal) : Backend.Goal :=
+  Backend.Goal.Goal g.id g.data (g.children.map (fun t => APITacticToBackendTactic t))
+termination_by (sizeOf g)
+decreasing_by
+  sorry
+
+def APITacticToBackendTactic (t : Tactic) : Backend.Tactic :=
+  Backend.Tactic.Tactic t.id t.data (t.children.map (fun g => APIGoaltoBackendGoal g))
+termination_by (sizeOf t)
+decreasing_by
+  sorry
+
+end
+
 -- TODO arg
 @[server_rpc_method]
 def getInitialState (_ : String) : RequestM (RequestTask Goal) :=
   -- TODO
-  RequestM.asTask $ pure
-    {
-      id:= "g0",
-      data:= "P /\\ Q", children:= [
-        {
-          id:= "t0",
-          data:= "split", children:= [
-            { id:= "g1", data:= "P", children:= []},
-            { id:= "g2", data:= "Q", children:= []},
-          ]
-        }
-      ]
-    }
+  RequestM.asTask $ pure $
+   backendGoalToAPIGoal (Backend.getInitialState "")
 
 @[server_rpc_method]
 def getSubgoals (t : Tactic) : RequestM (RequestTask (List Goal)) :=
   -- TODO
-  RequestM.pureTask $
-    if (t.id == "t0")
-      then
-        pure
-          [
-            {
-            id:= "g0",
-            data:= "P", children:= []},
-            {
-            id:= "g1",
-            data:= "Q", children:= []}
-            ]
-      else
-        if (t.id == "t1")
-          then
-            pure
-              []
-          else
-            pure
-              []
+  let t' := APITacticToBackendTactic t
+  let gs' := Backend.getSubgoals t'
+  RequestM.pureTask $ pure $ gs'.map (fun g => backendGoalToAPIGoal g)
 
 
 @[server_rpc_method]
 def getApplicableTactics (g : Goal) : RequestM (RequestTask (List Tactic)) :=
   -- TODO
-  RequestM.pureTask $
-    if (g.id == "g0")
-      then
-        pure
-          [
-            {
-            id:= "t0",
-            data:= "split", children:= []}
-            ]
-      else
-        if (g.id == "g1")
-          then
-            pure
-              [
-                {
-                id:= "t1",
-                data:= "exact P", children:= []}
-              ]
-          else
-            pure
-              [
-                {
-                id:= "t1",
-                data:= "exact Q", children:= []}
-              ]
+  let g' := APIGoaltoBackendGoal g
+  let ts' := Backend.getApplicableTactics g'
+  RequestM.pureTask $ pure $ ts'.map (fun t => backendTacticToAPITactic t)
 
 @[widget_module]
 def checkWidget : Widget.Module where
   javascript := include_str ".."/".lake"/"build"/"js"/"Hoverfly.js"
 
 #widget checkWidget
+
+
+
+namespace API
---
+++ Hoverfly/Backend.lean
@@ -0,0 +1,154 @@
+namespace Backend
+
+mutual
+
+inductive Goal : Type where
+| Goal (id : String) (data : String) (children : List Tactic) : Goal
+
+inductive Tactic : Type where
+| Tactic (id : String) (data : String) (children : List Goal) : Tactic
+
+end
+
+@[simp]
+theorem size_of_list_tactic (x : Tactic) (l : List Tactic) (h : x ∈ l) : sizeOf x <= sizeOf l := by
+  induction l <;> simp_all
+  cases h <;> simp_all <;> omega
+
+@[simp]
+theorem size_of_list_goal (x : Goal) (l : List Goal) (h : x ∈ l) : sizeOf x <= sizeOf l := by
+  induction l <;> simp_all
+  cases h <;> simp_all <;> omega
+
+mutual
+
+def sizeGoal (g : Goal) : Nat :=
+  match g with
+  | Goal.Goal _ _ cs =>
+      cs.foldl (fun acc t => acc + sizeTactic t) 1
+termination_by sizeOf g
+decreasing_by
+  simp_all
+  have ht : sizeOf t <= sizeOf cs := by simp_all
+  omega
+
+def sizeTactic (t : Tactic) : Nat :=
+  match t with
+  | Tactic.Tactic _ _ cs =>
+      cs.foldl (fun acc g => acc + sizeGoal g) 1
+termination_by sizeOf t
+decreasing_by
+  simp_all
+  have hg : sizeOf g <= sizeOf cs := by simp_all
+  omega
+
+end
+
+mutual
+
+@[simp]
+def depthGoal (g : Goal) : Nat :=
+  match g with
+  | Goal.Goal _ _ cs =>
+      let depths := cs.map (fun t => depthTactic t)
+      match h : depths with
+      | [] => 1
+      | t :: ts => 1 + (List.max depths (by simp_all))
+termination_by sizeOf g
+decreasing_by
+  simp_all
+  have ht : sizeOf t <= sizeOf cs := by simp_all
+  omega
+
+@[simp]
+def depthTactic (t : Tactic) : Nat :=
+  match t with
+  | Tactic.Tactic _ _ cs =>
+      let depths := cs.map (fun g => depthGoal g)
+      match h : depths with
+      | [] => 1
+      | g :: gs => 1 + (List.max depths (by simp_all))
+termination_by sizeOf t
+decreasing_by
+  simp_all
+  have hg : sizeOf g <= sizeOf cs := by simp_all
+  omega
+end
+
+@[simp]
+theorem depth_decreasing_goal
+  (id : String)
+  (data : String)
+  (t : Tactic)
+  (ts : List Tactic) :
+  t ∈ ts →
+  depthTactic t < depthGoal (Goal.Goal id data ts) := by
+  intros h
+  unfold depthGoal
+  cases ts <;> simp_all
+  conv =>
+    rhs
+    rhs
+    simp [← List.map_cons]
+  rw [Nat.add_comm]
+  rw [← List.mem_cons] at h
+  apply Nat.lt_add_one_of_le
+  apply List.le_max_of_mem
+  apply List.mem_map_of_mem
+  assumption
+
+@[simp]
+theorem depth_decreasing_tactic
+  (id : String)
+  (data : String)
+  (g : Goal)
+  (gs  : List Goal) :
+  g ∈ gs  →
+  depthGoal g < depthTactic (Tactic.Tactic id data gs) := by
+  intros h
+  unfold depthTactic
+  cases gs  <;> simp_all
+  conv =>
+    rhs
+    rhs
+    simp [← List.map_cons]
+  rw [Nat.add_comm]
+  rw [← List.mem_cons] at h
+  apply Nat.lt_add_one_of_le
+  apply List.le_max_of_mem
+  apply List.mem_map_of_mem
+  assumption
+
+open Lean
+
+--TODO
+def t0 := Tactic.Tactic "t0" "split" []
+def t1 := Tactic.Tactic "t1" "id" []
+def t2 := Tactic.Tactic "t2" "exact P" []
+def t3 := Tactic.Tactic "t3" "exact Q" []
+def g0 := Goal.Goal "g0" "P /\\ Q" [t0, t1]
+def g1 := Goal.Goal "g1" "P" []
+def g2 := Goal.Goal "g2" "Q" []
+def g3 := Goal.Goal "g3" "P /\\ Q" []
+
+-- TODO
+def getInitialState (_ : String) : Goal := g0
+
+def getSubgoals (t : Tactic) : List Goal :=
+  match t with
+  | Tactic.Tactic "t0" _ _ =>
+    [g1, g2]
+  | Tactic.Tactic "t1" _ _ => [g3]
+  | Tactic.Tactic "t2" _ _ => []
+  | Tactic.Tactic "t3" _ _ => []
+  | _ => []
+
+def getApplicableTactics (g : Goal) : List Tactic :=
+  match g with
+  | Goal.Goal "g0" _ _ => [t0, t1]
+  | Goal.Goal "g1" _ _ => [t2]
+  | Goal.Goal "g2" _ _ => [t3]
+  | Goal.Goal "g3" _ _ => []
+  | _ => []
+
+namespace Backend
