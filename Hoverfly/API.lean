1c50b5b2659f7d0f4298ada8ca1080b05818445f
cassiatorczon <cassiatorczon@gmail.com>
2026-02-05 21:25:49 -0500

    termination proofs

--- Hoverfly/API.lean
+++ Hoverfly/API.lean
@@ -59,17 +59,36 @@ theorem size_of_list_goal (x : Goal) (l : List Goal) (h : x ∈ l) : sizeOf x <=
   cases h <;> simp_all <;> omega
 
 mutual
+-- set_option pp.explicit true
 def APIGoaltoBackendGoal (g : Goal) : Backend.Goal :=
   Backend.Goal.Goal g.id g.data (g.children.map (fun t => APITacticToBackendTactic t))
 termination_by (sizeOf g)
 decreasing_by
-  sorry
+  cases g
+  simp_all
+  rw [Nat.add_assoc]
+  rw [Nat.add_assoc]
+  -- rw [Nat.add_comm]
+  rw [Nat.lt_one_add_iff]
+  rw [← Nat.add_assoc]
+  apply Nat.le_add_left_of_le
+  apply size_of_list_tactic
+  assumption
 
 def APITacticToBackendTactic (t : Tactic) : Backend.Tactic :=
   Backend.Tactic.Tactic t.id t.data (t.children.map (fun g => APIGoaltoBackendGoal g))
 termination_by (sizeOf t)
 decreasing_by
-  sorry
+  cases t
+  simp_all
+  rw [Nat.add_assoc]
+  rw [Nat.add_assoc]
+  -- rw [Nat.add_comm]
+  rw [Nat.lt_one_add_iff]
+  rw [← Nat.add_assoc]
+  apply Nat.le_add_left_of_le
+  apply size_of_list_goal
+  assumption
 
 end
 
@@ -83,24 +102,25 @@ def getInitialState (_ : String) : RequestM (RequestTask Goal) :=
 @[server_rpc_method]
 def getSubgoals (t : Tactic) : RequestM (RequestTask (List Goal)) :=
   -- TODO
-  let t' := APITacticToBackendTactic t
-  let gs' := Backend.getSubgoals t'
-  RequestM.pureTask $ pure $ gs'.map (fun g => backendGoalToAPIGoal g)
+  let tB := APITacticToBackendTactic t
+  let gsB := Backend.getSubgoals tB
+  let gs := gsB.map (fun g => backendGoalToAPIGoal g)
+  RequestM.pureTask $ pure $ gs
 
 
 @[server_rpc_method]
 def getApplicableTactics (g : Goal) : RequestM (RequestTask (List Tactic)) :=
   -- TODO
-  let g' := APIGoaltoBackendGoal g
-  let ts' := Backend.getApplicableTactics g'
-  RequestM.pureTask $ pure $ ts'.map (fun t => backendTacticToAPITactic t)
+  let gB := APIGoaltoBackendGoal g
+  let tsB := Backend.getApplicableTactics gB
+  let ts := tsB.map (fun t => backendTacticToAPITactic t)
+  RequestM.pureTask $ pure $ ts
 
 @[widget_module]
 def checkWidget : Widget.Module where
   javascript := include_str ".."/".lake"/"build"/"js"/"Hoverfly.js"
 
 #widget checkWidget
-
 
 
 namespace API
