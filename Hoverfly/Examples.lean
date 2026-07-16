1c844bec79641dc3a4084ad2ae0f21ed723d112b
cassiatorczon <cassiatorczon@gmail.com>
2026-07-16 16:10:54 -0400

    removed demos, added examples file

---
+++ Hoverfly/Examples.lean
@@ -0,0 +1,21 @@
+import Hoverfly.Backend
+
+set_option linter.unusedTactic false
+
+set_option linter.unreachableTactic false
+add_hoverfly_tactics
+  [
+    assumption,
+    contradiction,
+    intro,
+    rfl,
+    subst_eqs,
+    rewrite [Eq.comm],
+    rewrite [Nat.add_zero],
+    rewrite [Nat.add_succ],
+    apply Exists.intro,
+    apply Nat.le_trans,
+    apply Nat.zero_le,
+    apply Nat.sub_add_cancel,
+    rewrite [Nat.two_mul]
+  ]
