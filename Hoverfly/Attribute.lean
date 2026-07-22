ef202a2a7b68f8ce817aefcf9075279f3acea252
cassiatorczon <cassiatorczon@gmail.com>
2026-07-22 14:23:28 -0400

    drafted command to include all aesop rules as tactics

--- Hoverfly/Attribute.lean
+++ Hoverfly/Attribute.lean
@@ -1,6 +1,8 @@
 module
 
+public meta import Aesop.Frontend.Extension
 public meta import Lean.Elab.Command
+import Aesop
 import Batteries.Linter.UnreachableTactic
 
 public meta section
@@ -57,6 +59,50 @@ elab (name := addTacs)
   tacArray.forM (fun tac => modifyEnv fun env => hoverflyTacticExt.addEntry env tac)
 
 initialize Batteries.Linter.UnreachableTactic.addIgnoreTacticKind ``addTacs
+
+
+/- Aesop tactic integration -/
+def foobar : Syntax := Syntax.atom SourceInfo.none "need to implement this"
+
+open Aesop RuleTacDescr RuleTerm in
+def ruleTacDescrToStx (descr : RuleTacDescr) : CommandElabM (Option Syntax) :=
+  match descr with
+  | apply t md =>
+    match t with
+      | const decl =>
+        do return ((← `(tactic| apply $(mkIdent decl):ident)).raw)
+      | term t =>
+        do return ((← `(tactic| apply t)).raw) --TODO
+  | constructors (constructorNames : Array Name) md => return foobar--none --TODO
+  | forward (t : RuleTerm) (immediate : UnorderedArraySet PremiseIndex)
+      (isDestruct : Bool) => return foobar--none --TODO
+  | cases (target : CasesTarget) md
+      (isRecursiveType : Bool) (ctorNames : Array CtorNames) => return foobar--none --TODO
+  | tacticM (decl : Name) => return foobar--none --TODO
+  | ruleTac (decl : Name) => return foobar--none --TODO
+  | tacGen (decl : Name) => return foobar--none --TODO
+  | singleRuleTac (decl : Name) => return foobar--none --TODO
+  | tacticStx (stx : Syntax) => return (some stx)
+  | preprocess => return foobar--none --TODO
+  | forwardMatches (ms : Array ForwardRuleMatch) => return foobar--none --TODO
+
+open Aesop Aesop.Frontend in
+open Lean.Elab.Command in
+elab (name := addAesopTacs)
+    -- attrKind:attrKind
+    "add_aesop_tactics_to_hoverfly ": command => do
+  let aesopRuleSets ← liftCoreM getDeclaredGlobalRuleSets
+  for (setName, ruleSet, simpExtName, simprocExtName) in aesopRuleSets do
+    -- let forwardRules := (ruleSet.forwardRules.nameToRule.toList.map Prod.snd).map TODO
+    let normRuleDescrs := ruleSet.normRules.fold (fun rules rule => rule.tac :: rules) []
+    let safeRuleDescrs := ruleSet.safeRules.fold (fun rules rule => rule.tac :: rules) []
+    let unsafeRuleDescrs := ruleSet.unsafeRules.fold (fun rules rule => rule.tac :: rules) []
+    let ruleDescrs := normRuleDescrs ++ safeRuleDescrs ++ unsafeRuleDescrs
+    let rules ← ruleDescrs.filterMapM ruleTacDescrToStx
+    rules.forM (fun tac => modifyEnv fun env => hoverflyTacticExt.addEntry env {raw:=tac})
+
+initialize Batteries.Linter.UnreachableTactic.addIgnoreTacticKind ``addTacs
+
 
 /-
 -- -- Below is modified from Aesop :
