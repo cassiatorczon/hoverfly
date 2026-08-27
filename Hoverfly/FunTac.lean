f831a8f4c530bfaa0288c407c37f1f3bb7724d76
cassiatorczon <cassiatorczon@gmail.com>
2026-08-27 18:06:39 -0400

    comments and nits in funtac file

--- Hoverfly/FunTac.lean
+++ Hoverfly/FunTac.lean
@@ -18,10 +18,10 @@ public structure TacOutput where
   stx : Syntax -- syntax of the tactic
   goals : List MVarId -- goals resulting from the tactic application
   display : String -- pretty printed tactic
-  isNoop : Bool
-  solvesGoal : Bool
-  postState : SavedState
-  error : Option String := none
+  isNoop : Bool -- whether the tactic is a no-op
+  solvesGoal : Bool -- whether the tactic solves the goal
+  postState : SavedState -- state after the tactic application
+  error : Option String := none -- error thrown by tactic application
 
 public def errTacOutput (stx : Syntax) (display : String) (err : String)
   : Elab.TermElabM TacOutput := do
@@ -36,9 +36,9 @@ public def errTacOutput (stx : Syntax) (display : String) (err : String)
     error := some err}
 
 public def isErrTacOutput (output : TacOutput) : Bool :=
-  output.error.isSome || (output.goals == [] && !output.solvesGoal)
+  output.error.isSome --|| (output.goals == [] && !output.solvesGoal) TODO I think we don't need this part
 
 @[expose]
-public def FunTac := TacInput → TermElabM (List TacOutput) -- TODO
+public def FunTac := TacInput → TermElabM (List TacOutput)
 
 end FunTac
